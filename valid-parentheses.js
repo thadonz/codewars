288cdad0a7c87180542c4a11a454af9d0aa62cb0
Don Dao <dqdao@pbl.com>
2022-07-20 00:28:38 -0500

    remove charMap object

--- valid-parentheses.js
+++ valid-parentheses.js
@@ -1,12 +1,8 @@
 validParentheses = (parens) => {
     let count = 0; //if ever negative then it isn't valid
-    const charValues = {
-      '(': 1,
-      ')': -1
-    };
     
     for (let i = 0; i < parens.length && count >= 0; i++)  
-      count += charValues[parens.charAt(i)];
+      count += parens.charAt(i) === '(' ? 1 : -1;
 
     return count === 0;
   }
