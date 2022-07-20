95c31e8ade25e615bd80a93ab309e4ea81e7e632
Don Dao <dqdao@pbl.com>
2022-07-20 00:17:33 -0500

    use object to get values

--- valid-parentheses.js
+++ valid-parentheses.js
@@ -1,12 +1,13 @@
 validParentheses = (parens) => {
-  let count = 0; //if ever negative then it isn't valid
-  
-  for (let i = 0; i < parens.length && count >= 0; i++) {
-    let currChar = parens.charAt(i);
-    if(currChar === '(')
-      count++;
-    else if(currChar === ')')
-      count--;
+    let count = 0; //if ever negative then it isn't valid
+    const charValues = {
+      '(': 1,
+      ')': -1
+    };
+    
+    for (let i = 0; i < parens.length && count >= 0; i++) {
+      let currChar = parens.charAt(i);
+      count += charValues[currChar];
+    }
+    return count === 0;
   }
-  return count === 0;
-}
